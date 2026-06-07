63ee07e86223f8b24a0eea6539a397c2e20eeccc
hritikdangol <hritikdangol@gmail.com>
2026-06-07 12:54:39 +0545

    git commit -m "Updated volunteer login page, restaurant login and signup pages, index.html, and style.css for improved design and functionality."

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+function goToLogin() {
+    window.location.href = "/html/login.html";
+}
+
+function goToSignup() {
+    window.location.href = "/html/signup.html";
+}
+
+function goToVolunteerLogin() {
+    window.location.href = "/html/volunteerlogin.html";
+}
+
+function goToRestaurantLogin() {
+    window.location.href = "/html/restaurantlogin.html";
+}
+
+function goToRestaurantSignup() {
+    window.location.href = "html/restaurantsignup.html";
+}
+
+function showPage(page) {
+    alert(page);
+}
