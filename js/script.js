fe44c1a786175d378323ecc65869440cf49217bf
hritikdangol <hritikdangol@gmail.com>
2026-06-07 14:36:31 +0545

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
+const basePath = "/PROJECT-I";
+
 function goToLogin() {
-    window.location.href = "../html/login.html";
+    window.location.href = `${basePath}/html/login.html`;
 }
 
 function goToSignup() {
-    window.location.href = "../html/signup.html";
+    window.location.href = `${basePath}/html/signup.html`;
 }
 
 function goToVolunteerLogin() {
-    window.location.href = "../html/volunteerlogin.html";
+    window.location.href = `${basePath}/html/volunteerlogin.html`;
 }
 
 function goToRestaurantLogin() {
-    window.location.href = "../html/restaurantlogin.html";
+    window.location.href = `${basePath}/html/restaurantlogin.html`;
 }
 
 function goToRestaurantSignup() {
-    window.location.href = "../html/restaurantsignup.html";
+    window.location.href = `${basePath}/html/restaurantsignup.html`;
 }
 
 function showPage(page) {
