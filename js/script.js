7155b436d0ddbfa914cf88dcfed2314690dbbc55
hritikdangol <hritikdangol@gmail.com>
2026-06-07 21:05:22 +0545

    git commit -m "Update index.html, script.js, and style.css"

--- js/script.js
+++ js/script.js
@@ -1,23 +1,19 @@
 function goToLogin() {
-    window.location.href = "/html/login.html";
+    window.location.href = "/PROJECT-I/html/login.html";
 }
 
 function goToSignup() {
-    window.location.href = "/html/signup.html";
+    window.location.href = "/PROJECT-I/html/signup.html";
 }
 
 function goToVolunteerLogin() {
-    window.location.href = "/html/volunteerlogin.html";
+    window.location.href = "/PROJECT-I/html/volunteerlogin.html";
 }
 
 function goToRestaurantLogin() {
-    window.location.href = "/html/restaurantlogin.html";
+    window.location.href = "/PROJECT-I/html/restaurantlogin.html";
 }
 
 function goToRestaurantSignup() {
-    window.location.href = "html/restaurantsignup.html";
-}
-
-function showPage(page) {
-    alert(page);
+    window.location.href = "/PROJECT-I/html/restaurantsignup.html";
 }
