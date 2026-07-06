493f525c72a343a2445d4558a0ca42e647940bd1
hritikdangol <hritikdangol@gmail.com>
2026-07-06 21:21:10 +0545

    git commit -m "Added Font Awesome for icons, adjusted CSS for layout and spacing, and removed the Restaurant button from navigation."

--- js/slider.js
+++ js/slider.js
@@ -1,21 +1,25 @@
 const slides = document.querySelectorAll(".slide");
-let current = 0;
-function showSlide(index) {
 
+let current = 0;
+
+function showSlide(index) {
     slides.forEach(slide => {
         slide.classList.remove("active");
     });
 
     slides[index].classList.add("active");
-
-    bgEmoji.textContent = slides[index].dataset.emoji;
 }
 
+// Show the first slide
+showSlide(current);
+
+// Change slide every 4 seconds
 setInterval(() => {
     current++;
 
-    if(current >= slides.length){
+    if (current >= slides.length) {
         current = 0;
     }
+
     showSlide(current);
 }, 4000);
