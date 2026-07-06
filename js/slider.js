c25afc8c8d6cc8c4b59ca3c5efe5ec7367b590d7
Hrithik Dangol <hritikdangol@gmail.com>
2026-07-06 20:53:16 +0545

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,21 +1,24 @@
 const slides = document.querySelectorAll(".slide");
-let current = 0;
-function showSlide(index) {
 
+let current = 0;
+
+function showSlide(index) {
     slides.forEach(slide => {
         slide.classList.remove("active");
     });
 
     slides[index].classList.add("active");
-
-    bgEmoji.textContent = slides[index].dataset.emoji;
 }
 
+showSlide(current);
+
+// slide change garxa
 setInterval(() => {
     current++;
 
-    if(current >= slides.length){
+    if (current >= slides.length) {
         current = 0;
     }
+
     showSlide(current);
 }, 4000);
