6d89135d3dde00268083fd1a9953b6a2a19cd8c8
hritikdangol <hritikdangol@gmail.com>
2026-07-16 03:48:10 +0545

    git commit -m

--- js/signup.js
+++ js/signup.js
@@ -1,8 +1,7 @@
-function register(event) {
+function signup(event) {
     event.preventDefault();
 
-    const role = document.getElementById("role").value;
-    const fullname = document.getElementById("fullname").value;
+    const fullname = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const phone = document.getElementById("phone").value;
     const password = document.getElementById("password").value;
@@ -12,12 +11,13 @@ function register(event) {
         alert("Passwords do not match.");
         return;
     }
+
     if (localStorage.getItem(email)) {
         alert("Account already exists with this email.");
         return;
     }
+
     const user = {
-        role,
         fullname,
         email,
         phone,
@@ -26,6 +26,6 @@ function register(event) {
 
     localStorage.setItem(email, JSON.stringify(user));
 
-    alert("Registration successful!");
+    alert("User added successfully!");
     window.location.href = "login.html";
 }
